9641806aec7bf918aae460c76fbb843089b00170
Roger Agostinho <167251142+rogeragostinho@users.noreply.github.com>
2025-06-15 12:21:44 +0100

    update

--- PessoaFormDialog.java
+++ PessoaFormDialog.java
@@ -5,35 +5,90 @@ import java.awt.event.*;
 public abstract class PessoaFormDialog extends JDialog
 {
 	protected Pessoa pessoa;
+	private JLabel labelNome;
+	private JLabel labelIdade;
 	protected JTextField textFieldNome;
 	protected JTextField textFieldIdade;
 	private JButton buttonAdicionarPessoa;
+	private JPanel panelCenter;
 
 	public PessoaFormDialog(JFrame own, String titulo, Pessoa pessoa)
 	{
 		super(own, titulo, true);
-		setLayout(new GridLayout(5, 1));
+		setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
 
-		this.pessoa = pessoa;
+		this.pessoa = pessoa; 
+
 		initComponents();
 		addListeners();
 
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		setSize(400, 400);
+		setSize(300, 200);
 		setLocationRelativeTo(own);
 	}
 
 	public void initComponents()
 	{
+		GridBagLayout layout = new GridBagLayout();
+		panelCenter = new JPanel();
+		panelCenter.setLayout(layout);
+
+		GridBagConstraints constraints = new GridBagConstraints();
+
+		labelNome = new JLabel("Nome: ");
+		labelIdade = new JLabel("Idade: ");
+
 		initTextFields();
 		
 		buttonAdicionarPessoa = new JButton("Adicionar");
 
-		add(new JLabel("Nome: "));
-		add(textFieldNome);
-		add(new JLabel("Idade: "));
-		add(textFieldIdade);
-		add(buttonAdicionarPessoa);
+		// 
+		constraints.gridwidth = GridBagConstraints.REMAINDER;
+		constraints.anchor = GridBagConstraints.WEST;
+		layout.setConstraints(labelNome, constraints);
+		panelCenter.add(labelNome);
+		// 
+
+		// 
+		constraints.gridwidth = GridBagConstraints.REMAINDER;
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.weightx = 1;
+		layout.setConstraints(textFieldNome, constraints);
+		panelCenter.add(textFieldNome);
+		// 
+
+		// 
+		constraints.gridwidth = GridBagConstraints.REMAINDER;
+		constraints.anchor = GridBagConstraints.WEST;
+		constraints.insets = new Insets(15, 0, 0, 0);
+		layout.setConstraints(labelIdade, constraints);
+		panelCenter.add(labelIdade);
+		// 
+		
+		// 
+		constraints.gridwidth = GridBagConstraints.REMAINDER;
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.weightx = 1;
+		constraints.insets = new Insets(0, 0, 0, 0);
+		layout.setConstraints(textFieldIdade, constraints);
+		panelCenter.add(textFieldIdade);
+		// 
+
+		// ´
+		constraints.anchor = GridBagConstraints.CENTER;
+		constraints.gridwidth = GridBagConstraints.REMAINDER;
+		constraints.fill = GridBagConstraints.NONE;
+		constraints.weightx = 0;
+		constraints.insets = new Insets(15, 0, 0, 0);
+		layout.setConstraints(buttonAdicionarPessoa, constraints);
+		panelCenter.add(buttonAdicionarPessoa);
+		// 
+
+		//panelCenter.add(buttonAdicionarPessoa);
+
+		add(Box.createHorizontalStrut(40));
+		add(panelCenter);
+		add(Box.createHorizontalStrut(40));
 	}
 
 	public void initTextFields()
